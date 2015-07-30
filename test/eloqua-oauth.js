99091fc159e42e5c626dd65f5b27f162d9b4b45c
Oscar Fraxedas <oscar.fraxedas.tormo@oracle.com>
2015-07-30 09:13:39 -0400

    Updating the interface and adding more tests

--- test/eloqua-oauth.js
+++ test/eloqua-oauth.js
@@ -2,15 +2,93 @@
 var oauth = require('../index');
 var assert = require("assert");
 
+String.prototype.contains = function(other){
+    return this.indexOf(other) > -1;
+};
+
 describe('OAuth 2.0', function(){
 
-    describe('Authorization header', function(){
+    describe('Access header', function(){
 
+        var header = oauth.access_header('token');
+        
+        it('should contain authorization', function(){
+            assert.ok(header.Authorization);
+        });
+        
         it('should be bearer', function(){
-            var header = oauth.access_header('token');
-            assert.equal(header.Authorization,  'Bearer token');
+            assert.ok(header.Authorization.contains('Bearer'));
+        });
+        
+         it('should contain the token', function(){
+            assert.ok(header.Authorization.contains('token'));
+        });
+        
+        it('should be Bearer token', function(){
+            assert.equal(header.Authorization, 'Bearer token');
         });
 
+    });
+    
+    describe('Generate oauth authorize uri', function(){
+        
+        var uri = oauth.authorize_uri('client_id', 'redirect_uri', 'install_id');
+            
+        it('should contain all parameters', function(){
+            assert.ok(uri.contains('=client_id'));
+            assert.ok(uri.contains('=redirect_uri'));
+            assert.ok(uri.contains('=install_id'));
+        });
+    });
+    
+    describe('Grant options', function(){
+
+        var options = oauth.grant_request_options('client_id', 'client_secret', 'authorization_code', 'redirect_uri');
+         
+        describe('headers', function(){
+
+            var headers = options.headers;
+            
+            it('should contain authorization', function(){
+                assert.ok(headers.Authorization);
+            });
+            
+            it('should be basic', function(){
+                assert.ok(headers.Authorization.contains('Basic'));
+            });
+            
+        });
+        
+        describe('url', function(){
+
+            var url = options.url;
+            
+            it('should not be null', function(){
+                assert.ok(url);
+            });
+            
+            it('should be oauth', function(){
+                assert.ok(url.contains('oauth'));
+            });
+            
+        });
+        
+        describe('body', function(){
+
+            var json = options.json;
+            console.log(json);
+            
+            it('should not be json', function(){
+                assert.ok(json);
+            });
+            
+            it('should be have the proper values', function(){
+                assert.equal(json.grant_type, 'authorization_code');
+                assert.equal(json.code, 'authorization_code');
+                assert.equal(json.redirect_uri, 'redirect_uri');
+            });
+            
+        });
     });
 
 });
