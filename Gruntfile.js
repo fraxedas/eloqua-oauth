d3b5c39bc63349f020e62e5166ce8e00dac91e3d
Oscar Fraxedas <fraxedas@gmail.com>
2016-03-15 16:44:54 -0400

    working on coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,13 +8,33 @@ module.exports = function(grunt) {
         watch: {
             files: ['lib/**/*.js', 'test/**/*.js'],
             tasks : ['jshint']
+        },
+        coveralls: {
+            // Options relevant to all targets
+            options: {
+                // When true, grunt-coveralls will only print a warning rather than
+                // an error, to prevent CI builds from failing unnecessarily (e.g. if
+                // coveralls.io is down). Optional, defaults to false.
+                force: false
+            },
+            oauth: {
+                // LCOV coverage file (can be string, glob or array)
+                src: './coverage/*.info',
+                options: {
+                    // Any options for just this target
+                }
+            }
         }
     });
+    
+    
 
     // Load the plugin that provides the "jshint" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.loadNpmTasks('grunt-coveralls');
 
     // Default task(s).
     grunt.registerTask('default', ['watch']);
