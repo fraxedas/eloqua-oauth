a421447eb32bc2e4586a776fb304bfa5a9895adf
Oscar Fraxedas <fraxedas@gmail.com>
2016-03-21 21:28:37 -0400

    some cleanup

--- test/authentication.mock.js
+++ test/authentication.mock.js
@@ -1,28 +1,32 @@
 /// <reference path="../typings/mocha/mocha.d.ts"/>
-var rewire = require("rewire");
-var authentication = rewire('../lib/authentication');
-var assert = require("assert");
+describe('Authentication', function() {
+    
+    var assert = require('assert');
+    var authentication = require('../index').authentication;
 
-
-describe('OAuth 2.0', function() {
+    var request = {
+        post: function(options, next) {
+            if (options.fail) {
+                next(true);
+            }
+            else {
+                next(false, { statusCode: 302 }, { refresh_token: 1, access_token: 1 });
+            }
+        }
+    };
+    authentication.request = request;
 
     describe('Post request', function() {
 
-        var request = {
-            post: function(options, callback) {
-                if (option.fail) {
-                    callback(true);
-                }
-                else {
-                    callback(false, { statusCode: 302 }, { refresh_token: 1, access_token: 1 });
-                }
-            }
-        };
-        authentication.__set__("request", request);
-
         it('should return an error when the request fails', function() {
-            authentication.post_request({ fail: true }, function(error, response) {
+            authentication.post_request({ fail: true }, function(error, body) {
                 assert.ok(error);
+            });
+        });
+
+        it('should return the response when the request succeeds', function() {
+            authentication.post_request({ fail: false }, function(error, body) {
+                assert.ok(body);
             });
         });
 
